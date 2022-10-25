d6713d22f5237d36ace3060cb27b4f75eafecf45
jlkesh <john.lgd65@gmail.com>
2022-10-25 17:19:29 +0500

    initial

---
+++ src/main/java/uz/jl/pdfgenerator/springbootpdfgenerator/SpringBootPdfGeneratorApplication.java
@@ -0,0 +1,13 @@
+package uz.jl.pdfgenerator.springbootpdfgenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPdfGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPdfGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uz/jl/pdfgenerator/springbootpdfgenerator/SpringBootPdfGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.jl.pdfgenerator.springbootpdfgenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPdfGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
